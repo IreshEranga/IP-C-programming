295857d403d8aaae4f6b86f061e3045bc1568967
Iresh Eranga <117840288+Iresheranga@users.noreply.github.com>
2023-02-15 10:13:02 +0530

    Create 02.c

---
+++ tutorials/tutorial 01/Exercise 02/02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    float perimeter, area,length,width;
+    
+    length = 10;
+    width = 15.5;
+    
+    area = lenght * width;
+    perimeter = 2(length + width);
+    
+    printf("area = %.2f\n",area);
+    printf("perimeter = %.2f\n",perimeter);
+    return 0;
+}
